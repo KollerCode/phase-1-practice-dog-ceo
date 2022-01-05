018efc6701624a76ea9e461f9c54beeab3181c64
Natasha Koller <kollernatasha@gmail.com>
2022-01-05 15:30:34 -0500

    progress

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded', () => {
+	let imgContainer = document.getElementById("dog-image-container")
+
+	fetch('https://dog.ceo/api/breeds/image/random/4')
+	.then(response => response.json())
+	.then(jsonObject => {
+		let imagesArray = jsonObject.message
+	    imagesArray.forEach(url => imgContainer.innerHTML += makeImageURL(url))
+	})
+})
+
+function makeImageURL(url) {
+	return `<img src ="${url}"/>` 
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+	const dogBreed = document.getElementById("dog-breeds")
+	.createElement('li').appendChild('ul')
+    fetch('https://dog.ceo/api/breeds/list/all')
+	.then(res => res.json())
+	.then(dogBreedObject => {
+		let dogArray = dogBreedObject.message
+		dogArray.forEach(dog => dogBreed.innerHTML += ap)
+
+	})
+}

2a6b6a40e6fea3f2581dd007708391e56548c985
Natasha Koller <kollernatasha@gmail.com>
2022-01-05 21:09:04 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -21,11 +21,34 @@ document.addEventListener('DOMContentLoaded', () => {
 	.then(response => {
 		let dogArray = Object.keys(response.message)
 		dogArray.forEach((breed) => {
-			dogBreed.innerHTML += `<li>${breed}</li>`
+			dogBreed.innerHTML += `<li class="li-breed">${breed}</li>`
+		})
+		dogBreed.addEventListener('click', function(event){
+			if (event.target.tagName === "LI"){
+				event.target.style.color = 'pink'
+			}
+			else {
+				
+			}
 		})
 	})
-})
 
-function handleColorChange() {
-	addEventListener('click',)
-}
+	let dogSelect = document.getElementById("breed-dropdown")
+	dogSelect.addEventListener('change', (event) => {
+		fetch('https://dog.ceo/api/breeds/list/all')
+		.then(res => res.json())
+		.then(res => {
+		let dogArray = Object.keys(res.message)
+		let filteredArray = dogArray.filter(breed => {
+			return breed.startsWith(event.target.value)
+		})
+
+		dogBreed.innerHTML = ''
+			
+		filteredArray.forEach((breed) => {
+			dogBreed.innerHTML += `<li class="li-breed">${breed}</li>`
+		})
+
+	})
+	})
+})
